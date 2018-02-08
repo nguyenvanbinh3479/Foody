3724a42a7f241584d2c679a6c1d473cff9fa5841
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-02-08 15:45:01 +0700

    Edited layout

--- app/src/main/java/com/example/binh_pc/foody/Main3Activity.java
+++ app/src/main/java/com/example/binh_pc/foody/Main3Activity.java
@@ -3,7 +3,7 @@ package com.example.binh_pc.foody;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Main3Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
